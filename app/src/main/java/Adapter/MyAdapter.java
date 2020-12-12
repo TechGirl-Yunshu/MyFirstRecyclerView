8ae5aa17dbe9574e21a965339bea763170a458df
Gisele Xiao <yunshu0507@gmail.com>
2020-12-12 13:37:22 -0500

    Enable scroll view

--- app/src/main/java/Adapter/MyAdapter.java
+++ app/src/main/java/Adapter/MyAdapter.java
@@ -1,11 +1,15 @@
 package Adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myrecyclerview.R;
 
 import java.util.List;
 
@@ -14,30 +18,41 @@ import Model.ListItem;
 public class
 MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     private Context context;
-    private List<ListItem> listItems;
+    private List<ListItem> listItem;
     public MyAdapter(Context context, List listItem){
+        this.context = context;
+        this.listItem = listItem;
 
     }
 
     @NonNull
     @Override
-    public MyAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_row, parent,false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyAdapter.ViewHolder holder, int position) {
+        ListItem item = listItem.get(position);
+        holder.name.setText(item.getName());
+        holder.description.setText(item.getDescription());
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listItem.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView name;
+        public TextView description;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            name = (TextView) itemView.findViewById(R.id.title);
+            description = (TextView) itemView.findViewById(R.id.description);
         }
     }
 }
